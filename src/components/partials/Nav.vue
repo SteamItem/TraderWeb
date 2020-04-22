2b6f69d67e8cf318143bf2a28a50c8a510c8e970
Harun Sokullu <harunsokullu@gmail.com>
2020-04-23 01:46:20 +0300

    update project name

--- src/components/partials/Nav.vue
+++ src/components/partials/Nav.vue
@@ -2,7 +2,7 @@
 <nav class="navbar container" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="/">
-      <strong class="is-size-4">CSGO Trader</strong>
+      <strong class="is-size-4">Trader Bot</strong>
     </a>
   </div>
   <div id="navbar" class="navbar-menu">
