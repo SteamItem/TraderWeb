4a19a478f5cee05df13a2d76cb3f19e0fdb71cae
Harun Sokullu <harunsokullu@gmail.com>
2020-10-08 01:35:52 +0300

    remove fav

--- src/components/Rollbit.vue
+++ src/components/Rollbit.vue
@@ -26,37 +26,7 @@
           </v-btn>
         </v-toolbar>
       </template>
-      <template v-slot:item.actions="{ item }">
-        <v-checkbox v-model="item.fav" @change="historyItemFavChange(item.name, item.fav)"></v-checkbox>
-      </template>
     </v-data-table>
-    <v-data-table
-      :headers="favHeaders"
-      :items="favItems"
-      :search="favSearch"
-      sort-by="name"
-      class="elevation-1"
-    >
-      <template v-slot:top>
-        <v-toolbar flat color="white">
-          <v-toolbar-title>Rollbit Fav</v-toolbar-title>
-          <v-text-field
-            v-model="favSearch"
-            append-icon="mdi-magnify"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-          <v-spacer></v-spacer>
-          <v-btn class="mx-2" fab dark color="indigo" small @click="favFindAll">
-            <v-icon dark>mdi-refresh</v-icon>
-          </v-btn>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.actions="{ item }">
-        <v-btn color="red" small @click="historyItemFavChange(item.name, false)">Delete</v-btn>
-      </template>
-  </v-data-table>
   </v-card>
 </template>
 
@@ -72,15 +42,8 @@
         { text: 'Base Price', value: 'baseprice' },
         { text: 'Markup', value: 'markup' },
         { text: 'Price', value: 'price' },
-        { text: 'Fav', value: 'actions', sortable: false },
-      ],
-      favSearch: '',
-      favHeaders: [
-        { text: 'Item Name', value: 'name' },
-        { text: 'Delete', value: 'actions', sortable: false },
       ],
       historyItems: [],
-      favItems: [],
     }),
     created () {
       this.findAll()
@@ -88,24 +51,10 @@
     methods: {
       findAll() {
         this.historyFindAll();
-        this.favFindAll();
       },
       async historyFindAll() {
         var response = await axios.get(`${process.env.VUE_APP_API_URL}/rollbit`)
         this.historyItems = response.data;
-      },
-      async favFindAll() {
-        var response = await axios.get(`${process.env.VUE_APP_API_URL}/rollbitFav`)
-        this.favItems = response.data;
-      },
-      async historyItemFavChange(itemName, fav) {
-        let postData = {name: itemName};
-        if(fav) {
-          await axios.post(`${process.env.VUE_APP_API_URL}/rollbitFav/add`, postData);
-        } else {
-          await axios.post(`${process.env.VUE_APP_API_URL}/rollbitFav/remove`, postData);
-        }
-        this.findAll();
       }
     },
   }
